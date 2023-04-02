9697cbba90bc22c320e0f63da1cfd65fe7d6c062
parapsychic <febinkdominic@outlook.com>
2023-04-02 22:17:31 +0530

    Basic

---
+++ client/src/Card.jsx
@@ -0,0 +1,16 @@
+import "./styles/Card.css";
+
+export default function Card({ profilePicture, name, text, image }) {
+  return (
+    <div className="feed-container">
+      <div className="user-bar">
+        <img className="profile-picture" src={profilePicture}></img>
+        <div className="name">{name}</div>
+      </div>
+      <div className="text">
+        {text}
+      </div>
+      <img className="user-media" src={image}></img>
+    </div>
+  )
+}
